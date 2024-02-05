5a72a1f98e65b9ab544e5bc20ff8bc30472ae456
FireIsGood <fireisadequate@gmail.com>
2024-02-05 01:28:14 -0800

    Combine workspace related plugins

--- .config/nvim/lua/plugins/sessions.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "natecraddock/sessions.nvim",
-  config = function()
-    require("sessions").setup({
-      session_filepath = vim.fn.stdpath("data") .. "/sessions",
-      absolute = true,
-    })
-  end,
-}
--- .config/nvim/lua/plugins/workspaces.lua
+++ .config/nvim/lua/plugins/workspaces.lua
@@ -1,32 +1,28 @@
----@diagnostic disable-next-line: unused-function
-local function open_tree()
-  vim.cmd("Neotree") -- Open neotree
-  vim.cmd("winc p") -- Move to the first tab on the right
-
-  -- Close the random annoying empty file
-  if vim.api.nvim_buf_get_name(0) == "" then
-    -- print("Nerd annhihilated")
-    require("mini.bufremove").delete()
-  else
-    -- print("No need, no nerd detected.")
-  end
-end
-
 return {
-  "natecraddock/workspaces.nvim",
-  lazy = false,
-  config = function()
-    require("workspaces").setup({
-      hooks = {
-        open_pre = function()
-          vim.cmd("SessionsStop")
-          vim.cmd("silent %bdelete!")
-        end,
-        open = function()
-          vim.cmd("SessionsLoad") -- Load a saved session
-          -- open_tree()
-        end,
-      },
-    })
-  end,
+  {
+    "natecraddock/workspaces.nvim",
+    lazy = false,
+    config = function()
+      require("workspaces").setup({
+        hooks = {
+          open_pre = function()
+            vim.cmd("SessionsStop") -- Stops recording the session
+            vim.cmd("silent %bdelete!") -- Closes all tabs
+          end,
+          open = function()
+            vim.cmd("SessionsLoad") -- Load a saved session
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "natecraddock/sessions.nvim",
+    config = function()
+      require("sessions").setup({
+        session_filepath = vim.fn.stdpath("data") .. "/sessions", -- Saved at ~/.local/share/nvim/sessions/
+        absolute = true,
+      })
+    end,
+  },
 }
