ebfda19f398b6685189d9eaf0032b8d75e3ad4a4
FireIsGood <fireisadequate@gmail.com>
2024-05-18 13:25:00 -0700

    Disable tailwind
    Agony

--- .config/nvim/lua/plugins/tailwind-sorter.lua
+++ .config/nvim/lua/plugins/tailwind-sorter.lua
@@ -1,5 +1,9 @@
+if true then
+  return {}
+end
 return {
   "laytan/tailwind-sorter.nvim",
+  lazy = true,
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
   build = "cd formatter && npm i && npm run build",
   config = true,
