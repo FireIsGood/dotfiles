5665a99209382a0a5d931b324930029bdae3228d
FireIsGood <fireisadequate@gmail.com>
2024-05-18 14:29:37 -0700

    Disable indent scope highlight

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -2,7 +2,7 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   commit = "3d08501caef2329aba5121b753e903904088f7e6",
   opts = {
-    scope = { highlight = "Ibl-custom-hl" },
+    scope = { enabled = false, highlight = "Ibl-custom-hl" },
     indent = {
       char = "┆",
     },
