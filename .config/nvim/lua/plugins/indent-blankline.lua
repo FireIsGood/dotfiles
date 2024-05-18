3159edcd7e32ab0aecefecb65e0d9cbbc8852919
FireIsGood <fireisadequate@gmail.com>
2024-05-18 14:27:08 -0700

    Change indent blankline to be lower contrast

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,4 +1,13 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   commit = "3d08501caef2329aba5121b753e903904088f7e6",
+  opts = {
+    scope = { highlight = "Ibl-custom-hl" },
+    indent = {
+      char = "┆",
+    },
+  },
+  init = function()
+    vim.api.nvim_set_hl(0, "Ibl-custom-hl", { fg = "#6e738d" })
+  end,
 }
