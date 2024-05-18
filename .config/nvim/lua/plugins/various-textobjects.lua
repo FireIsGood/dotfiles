226f7df587e8115a43135cd2e32f9fd7445488f7
FireIsGood <fireisadequate@gmail.com>
2024-05-17 17:36:06 -0700

    Fix various text objects config

--- .config/nvim/lua/plugins/various-textobjects.lua
+++ .config/nvim/lua/plugins/various-textobjects.lua
@@ -1,7 +1,5 @@
 return {
   "chrisgrieser/nvim-various-textobjs",
-  lazy = true,
-  config = function()
-    require("various-textobjs").setup({})
-  end,
+  lazy = false,
+  opts = { useDefaultKeymaps = true },
 }
