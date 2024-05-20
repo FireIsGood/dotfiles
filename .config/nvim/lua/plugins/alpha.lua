c0beb174bd05d2dd1e58aeccfcdd28832d5c5470
FireIsGood <fireisadequate@gmail.com>
2024-05-20 03:55:43 -0700

    Update dashboard options

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -17,13 +17,13 @@ return {
     -- stylua: ignore
     dashboard.section.buttons.val = {
       dashboard.button("w", "󱎃 " .. " Telescope workspaces",   "<cmd> Telescope workspaces <cr>"),
-      dashboard.button("f", " " .. " Find file",              "<cmd> Telescope find_files <cr>"),
+      -- dashboard.button("f", " " .. " Find file",              "<cmd> Telescope find_files <cr>"),
       -- dashboard.button("n", " " .. " New file",               "<cmd> ene <BAR> startinsert <cr>"),
-      dashboard.button("r", " " .. " Recent files",           "<cmd> Telescope oldfiles <cr>"),
-      dashboard.button("g", " " .. " Find text",              "<cmd> Telescope live_grep <cr>"),
+      -- dashboard.button("r", " " .. " Recent files",           "<cmd> Telescope oldfiles <cr>"),
+      -- dashboard.button("g", " " .. " Find text",              "<cmd> Telescope live_grep <cr>"),
       -- dashboard.button("c", " " .. " Config",                 "<cmd> lua require('lazyvim.util').telescope.config_files()() <cr>"),
       -- dashboard.button("s", " " .. " Restore Session",        [[<cmd> lua require("persistence").load() <cr>]]),
-      dashboard.button("x", " " .. " Lazy Extras",            "<cmd> LazyExtras <cr>"),
+      -- dashboard.button("x", " " .. " Lazy Extras",            "<cmd> LazyExtras <cr>"),
       dashboard.button("l", "󰒲 " .. " Lazy",                   "<cmd> Lazy <cr>"),
       dashboard.button("q", " " .. " Quit",                   "<cmd> qa <cr>"),
     }
