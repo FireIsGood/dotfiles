0aef5b1c31b199c63e364c67b0e82bbd57446c37
FireIsGood <fireisadequate@gmail.com>
2024-05-20 03:55:50 -0700

    Enable Oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,24 @@
-if true then
-  return {}
-end
 return {
   "stevearc/oil.nvim",
-  opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup()
-  end,
+  opts = {
+    default_file_explorer = false,
+    delete_to_trash = true,
+    constrain_cursor = "name",
+    keymaps = {
+      ["g?"] = "actions.show_help",
+      ["<CR>"] = "actions.select",
+      ["<C-c>"] = "actions.close",
+      ["<C-l>"] = "actions.refresh",
+      ["-"] = "actions.parent",
+    },
+    use_default_keymaps = false,
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      max_width = 120,
+      max_height = 40,
+    },
+  },
 }
