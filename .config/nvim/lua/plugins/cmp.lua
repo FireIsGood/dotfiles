abafc1e132efa9fafd8c43b41fea527feab41be5
FireIsGood <fireisadequate@gmail.com>
2024-05-19 23:15:44 -0700

    Refactor cmp sources

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,11 +1,9 @@
 return {
   "hrsh7th/nvim-cmp",
-  dependencies = { "hrsh7th/cmp-emoji", "nat-418/cmp-color-names.nvim" },
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    table.insert(opts.sources, { { name = "emoji" }, { name = "color_names" } })
-    opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<C-y>"] = require("cmp").mapping.complete(),
-    })
-  end,
+  dependencies = { "hrsh7th/cmp-emoji" },
+  opts = {
+    sources = {
+      { name = "emoji" },
+    },
+  },
 }
