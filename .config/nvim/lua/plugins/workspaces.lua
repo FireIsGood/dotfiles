8a8ec3a6a72bad76811fe68501e48e4e4704898e
FireIsGood <fireisadequate@gmail.com>
2024-05-19 23:16:01 -0700

    Refactor workspaces options

--- .config/nvim/lua/plugins/workspaces.lua
+++ .config/nvim/lua/plugins/workspaces.lua
@@ -2,30 +2,24 @@ return {
   {
     "natecraddock/workspaces.nvim",
     lazy = false,
-    config = function()
-      require("workspaces").setup({
-        hooks = {
-          open_pre = function()
-            vim.cmd("SessionsStop") -- Stops recording the session
-            if #vim.api.nvim_list_wins() > 1 then
-              vim.cmd("winc h") -- Go to a real buffer (get outta the file explorer!)
-            end
-            vim.cmd("silent %bdelete!") -- Closes all tabs
-          end,
-          open = function()
-            vim.cmd("SessionsLoad") -- Load a saved session
-          end,
-        },
-      })
-    end,
+    opts = {
+      hooks = {
+        open_pre = function()
+          vim.cmd("SessionsStop") -- Stops recording the session
+          vim.cmd("Neotree close") -- Close Neo tree
+          vim.cmd("silent %bdelete!") -- Closes all tabs
+        end,
+        open = function()
+          vim.cmd("SessionsLoad") -- Load a saved session
+        end,
+      },
+    },
   },
   {
     "natecraddock/sessions.nvim",
-    config = function()
-      require("sessions").setup({
-        session_filepath = vim.fn.stdpath("data") .. "/sessions", -- Saved at ~/.local/share/nvim/sessions/
-        absolute = true,
-      })
-    end,
+    opts = {
+      session_filepath = vim.fn.stdpath("data") .. "/sessions", -- Saved at ~/.local/share/nvim/sessions/
+      absolute = true,
+    },
   },
 }
