4faa43debb33975bf562551c1ba580ccbd17b92f
FireIsGood <fireisadequate@gmail.com>
2024-05-20 20:42:02 -0700

    Fix cmp sources

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,9 +1,9 @@
 return {
   "hrsh7th/nvim-cmp",
-  dependencies = { "hrsh7th/cmp-emoji" },
-  opts = {
-    sources = {
-      { name = "emoji" },
-    },
-  },
+  dependencies = { "hrsh7th/cmp-emoji", "nat-418/cmp-color-names.nvim" },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    local sources = { { name = "emoji" }, { name = "color_names" } }
+    opts.sources = vim.tbl_deep_extend("force", opts.sources, sources)
+  end,
 }
