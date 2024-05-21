cff439f0ec9fc741abb7d6b8d0f1e8a66e465c63
FireIsGood <fireisadequate@gmail.com>
2024-05-20 20:42:09 -0700

    Enable origami again

--- .config/nvim/lua/plugins/origami.lua
+++ .config/nvim/lua/plugins/origami.lua
@@ -1,6 +1,6 @@
-if true then
-  return {}
-end
+-- if true then
+--   return {}
+-- end
 return {
   "chrisgrieser/nvim-origami",
   event = "BufReadPost", -- later or on keypress would prevent saving folds
