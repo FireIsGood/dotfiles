ea5ec9c367630d7624eeb3d1c2be97f449d7c103
FireIsGood <fireisadequate@gmail.com>
2024-05-24 16:10:02 -0700

    feat: change typoslsp severity and add config

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,7 +1,15 @@
 return {
   "neovim/nvim-lspconfig",
   ---@class PluginLspOpts
-  opts = { document_highlight = { enabled = false } },
+  opts = {
+    document_highlight = { enabled = false },
+    servers = {
+      typos_lsp = {
+        config = "~/code/typos-lsp/crates/typos-lsp/tests/typos.toml",
+        init_options = { diagnosticSeverity = "information" },
+      },
+    },
+  },
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     -- change a keymap
