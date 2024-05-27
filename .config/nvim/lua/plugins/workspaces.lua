e20e4ebe5621bb6ccbf59ecf8cc557b86cb7dedb
FireIsGood <fireisadequate@gmail.com>
2024-05-27 16:17:54 -0700

    feat: disable workspaces

--- .config/nvim/lua/plugins/workspaces.lua
+++ .config/nvim/lua/plugins/workspaces.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    "natecraddock/workspaces.nvim",
-    lazy = false,
-    opts = {
-      hooks = {
-        open_pre = function()
-          vim.cmd("SessionsStop") -- Stops recording the session
-          vim.cmd("Neotree close") -- Close Neo tree
-          vim.cmd("silent %bdelete!") -- Closes all tabs
-        end,
-        open = function()
-          vim.cmd("SessionsLoad") -- Load a saved session
-        end,
-      },
-    },
-  },
+  -- {
+  --   "natecraddock/workspaces.nvim",
+  --   lazy = false,
+  --   opts = {
+  --     hooks = {
+  --       open_pre = function()
+  --         vim.cmd("SessionsStop") -- Stops recording the session
+  --         vim.cmd("Neotree close") -- Close Neo tree
+  --         vim.cmd("silent %bdelete!") -- Closes all tabs
+  --       end,
+  --       open = function()
+  --         vim.cmd("SessionsLoad") -- Load a saved session
+  --       end,
+  --     },
+  --   },
+  -- },
   {
     "natecraddock/sessions.nvim",
     opts = {
