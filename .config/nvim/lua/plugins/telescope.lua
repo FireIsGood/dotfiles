cc8366753ea6337b8506e3968a8e023f91e697b2
FireIsGood <fireisadequate@gmail.com>
2024-06-12 00:37:21 -0700

    feat: add git file history Telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,9 @@ local Util = require("lazyvim.util")
 
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    { "isak102/telescope-git-file-history.nvim", dependencies = { "tpope/vim-fugitive" } },
+  },
   lazy = false,
   opts = {
     pickers = {
@@ -17,4 +20,7 @@ return {
     { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
     { "<leader>fO", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)" },
   },
+  init = function()
+    require("telescope").load_extension("git_file_history")
+  end,
 }
