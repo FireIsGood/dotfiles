d42596cc7901aefdb1880349fd7c5c9462ac5d4f
FireIsGood <fireisadequate@gmail.com>
2024-11-29 17:01:32 -0800

    feat: change mini-surround mappings

--- .config/nvim/lua/plugins/mini-surround.lua
+++ .config/nvim/lua/plugins/mini-surround.lua
@@ -3,13 +3,13 @@ return {
   lazy = false,
   opts = {
     mappings = {
-      add = "gza", -- Add surrounding in Normal and Visual modes
-      delete = "gzd", -- Delete surrounding
-      find = "gzf", -- Find surrounding (to the right)
-      find_left = "gzF", -- Find surrounding (to the left)
-      highlight = "gzh", -- Highlight surrounding
-      replace = "gzr", -- Replace surrounding
-      update_n_lines = "gzn", -- Update `n_lines`
+      add = "gs", -- Add surrounding in Normal and Visual modes
+      delete = nil, -- Delete surrounding
+      find = nil, -- Find surrounding (to the right)
+      find_left = nil, -- Find surrounding (to the left)
+      highlight = nil, -- Highlight surrounding
+      replace = nil, -- Replace surrounding
+      update_n_lines = nil, -- Update `n_lines`
     },
   },
 }
