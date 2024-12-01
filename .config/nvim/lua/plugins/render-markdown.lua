3bb18aa28779eca7464f5eda2e129fa228c039d0
FireIsGood <fireisadequate@gmail.com>
2024-12-01 01:25:38 -0800

    feat: render markdown settings

---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,19 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    render_modes = { "n", "c", "i" },
+    heading = {
+      position = "inline",
+      icons = { "𝟏 ", "𝟐 ", "𝟑 ", "𝟒 ", "𝟓 ", "𝟔 " },
+      width = "block",
+    },
+    code = {
+      language_name = false,
+      above = "█",
+      below = "█",
+    },
+    dash = {
+      width = 120,
+    },
+  },
+}
